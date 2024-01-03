d3b99e17304f34f56df78d1b7f5f5076936acb7c
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-01-03 22:11:51 +0530

    localStroge

--- localStorage.js
+++ localStorage.js
@@ -1,25 +1,16 @@
 
 function handleFormSubmit(event) {
     event.preventDefault(); 
+    
 
-    // Get form values
-    var username = document.getElementById('username').value;
-    var email = document.getElementById('email').value;
-    var phoneNumber = document.getElementById('phoneNumber').value;
+    const userName   = document.getElementById('username').value
+    const userEmail = document.getElementById('email').value
+    const userPhone = document.getElementById('phoneNumber').value
 
-    // Create an object to store user details
-    var userDetails = {
-        username: username,
-        email: email,
-        phoneNumber: phoneNumber
-    };
-
-    // Convert the object to a JSON string and store in local storage
-    localStorage.setItem('userDetails', JSON.stringify(userDetails));
-
-    // Optionally, you can display a confirmation message or redirect to another page
-    alert('User details have been successfully stored in local storage.');
+    localStorage.setItem('name', userName)
+    localStorage.setItem('email',userEmail)
+    localStorage.setItem('phoneNumber',userPhone)
+    console.log(localStorage.getItem('name'))
+    console.log(localStorage.getItem('email'))
+    console.log(localStorage.getItem('phoneNumber'))
 }
-
-// Attach the handleFormSubmit function to the form's submit event
-document.getElementById('userForm').addEventListener('submit', handleFormSubmit);
